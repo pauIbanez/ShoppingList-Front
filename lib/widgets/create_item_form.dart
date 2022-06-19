f2de0cbccaf6298e85b8d213510333ac2e84fb02
Pau Ibáñez <pauibanez2001@gmail.com>
2022-06-19 19:27:18 +0200

    created the form fields and validators

--- lib/widgets/create_item_form.dart
+++ lib/widgets/create_item_form.dart
@@ -9,8 +9,73 @@ class CreateItemForm extends StatefulWidget {
 }
 
 class _CreateItemFormState extends State<CreateItemForm> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _name = "";
+  int _quantity = 1;
+
+
+  Widget _buildNameField() {
+    return TextFormField(
+      autofocus: true,
+      decoration: const InputDecoration(labelText: "Name"),
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          return "Name is required";
+        }
+        return null;
+      },
+      onSaved: (String? value) {
+        _name = value ?? "";
+      },
+    );
+  }
+
+  Widget _buildQuantityField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Quantity"),
+      keyboardType: TextInputType.number,
+      initialValue: "1",
+      validator: (String? value) {
+        if(value!.isEmpty){
+          return "Quantity can't be empty";
+        }
+
+        if(value == "0"){
+          return "Quantity must be 1 or more";
+        }
+        
+        if(int.tryParse(value) == null){
+          return "Quantity must be a number";
+        }
+
+        _quantity =  int.tryParse(value) ?? 1;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          _buildNameField(),
+          _buildQuantityField(),
+          SizedBox(height: 100),
+          ElevatedButton(
+            onPressed: (() {
+              if(!_formKey.currentState!.validate()){
+                return;
+              }
+
+              _formKey.currentState!.save();
+            }),
+            child: const Text("Create", style: TextStyle(color: Colors.white))
+          )
+        ],
+      )
+    );
   }
 }
